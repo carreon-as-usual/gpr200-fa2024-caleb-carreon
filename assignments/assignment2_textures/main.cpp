7d9c245492e4e4d2569f3f28161a9eb81980f6d4
Caleb Carreon <calebcarreon@gmail.com>
2024-09-26 01:22:31 -0400

    Assignment 2 started

---
+++ assignments/assignment2_textures/main.cpp
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+#include <ew/external/glad.h>
+#include <ew/ewMath/ewMath.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <ccarreon/shader.h>
+#include <ew/external/stb_image.cpp>
+
+const int SCREEN_WIDTH = 1080;
+const int SCREEN_HEIGHT = 720;
+
+float vertices[] = {
+	// X     Y     Z     R      G     B    A
+	 0.5f,  0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f,
+	 0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f,
+	-0.5f, -0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f,
+	-0.5f,  0.5f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f
+};
+unsigned int indices[] = {
+	0, 1, 3,
+	1, 2, 3
+};
+int width, height, nrChannels;
+unsigned char* data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+int main() {
+	printf("Initializing...");
+	if (!glfwInit()) {
+		printf("GLFW failed to init!");
+		return 1;
+	}
+	GLFWwindow* window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello Triangle", NULL, NULL);
+	if (window == NULL) {
+		printf("GLFW failed to create window");
+		return 1;
+	}
+	glfwMakeContextCurrent(window);
+	if (!gladLoadGL(glfwGetProcAddress)) {
+		printf("GLAD Failed to load GL headers");
+		return 1;
+	}
+	//Initialization goes here!
+	unsigned int VAO;
+	unsigned int VBO;
+	unsigned int EBO;
+
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	//NEWER OPTION:  glNamedBufferData(VBO, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	//POSITION (XYZ)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	
+	//COLOR (RGBA)
+	glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 7 * sizeof(float), (void*)(sizeof(float) * 3));
+	glEnableVertexAttribArray(1);
+
+	ccarreon::Shader shader("assets/vertexShader.vert", "assets/fragmentShader.frag");
+
+	unsigned int texture;
+	glGenTextures(1, &texture);
+
+	glBindTexture(GL_TEXTURE_2D, texture);
+
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glGenerateMipmap(GL_TEXTURE_2D);
+
+	//Render loop
+	while (!glfwWindowShouldClose(window)) {
+		glfwPollEvents();
+		float time = (float)glfwGetTime();
+		//Clear framebuffer
+		glClearColor(0.3f, 0.4f, 0.9f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		//Drawing happens here!
+		shader.use();
+
+		// Set time uniform
+		shader.setFloat("uTime", time);
+
+		glBindVertexArray(VAO);
+
+		//Draw call
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+		glfwSwapBuffers(window);
+	}
+	printf("Shutting down...");
+}
